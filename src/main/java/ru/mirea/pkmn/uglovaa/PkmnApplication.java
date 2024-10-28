05652f30b9e2a78a900a77067fbb5c9bb2048c1f
UtwoA <loskon64@gmail.com>
2024-10-28 15:46:10 +0300

    new files

--- src/main/java/ru/mirea/pkmn/uglovaa/PkmnApplication.java
+++ src/main/java/ru/mirea/pkmn/uglovaa/PkmnApplication.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 public class PkmnApplication {
     public static final long serialVersionUID = 1L;
     public static void main(String[] args) throws IOException {
-        `
+
         CardImport imp = new CardImport();
         //Card card = imp.importCards(".\\src\\main\\resources\\my_card.txt");
 
